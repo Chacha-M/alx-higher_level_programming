5f667b91df8c83d46597140149324d33964bd005
Charity Muteti <107112309+Chacha-M@users.noreply.github.com>
2023-06-14 12:09:31 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,17 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-const url = process.argv[2];
-request.get(url, function (err, response, body) {
-  let count = 0;
-  if (err) {
-    console.log(err);
-  }
-  const data = JSON.parse(body);
-  for (let i = 0; data.results[i] !== undefined; i++) {
-    if (data.results[i].characters.includes('https://swapi-api.alx-tools.com/api/films/')) {
-      count++;
+
+if (process.argv.length > 2) {
+  request(`${process.argv[2]}`, (err, res, body) => {
+    if (err) {
+      console.log(err);
+    } else if (body) {
+      const charFilms = JSON.parse(body).results.filter(
+        x => x.characters.find(y => y.match(/\/people\/18\/?$/))
+      );
+
+      console.log(charFilms.length);
     }
-  }
-  console.log(count);
-});
+  });
+}
